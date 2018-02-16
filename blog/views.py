4acb32496f7a02886fa036b3f00b829fd2860ef5
mahmoud ibrahem <mahmoud.ibrahem12596@gmail.com>
2018-02-16 08:59:27 +0200

    Making our first view (edit the views py)

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import post
 from django.shortcuts import render
 
 # Create your views here.
+
+def list_of_post(request):
+    post1=post.objects.all()
+    template={'blog/post/list_of_post.html'}
+    context={'post': post1}
+    return  render(request ,template,context)
+
